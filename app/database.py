f20f12613410df84cd0e8e0d926b0409c6c96b30
admin <admin@djangogirls.com>
2023-01-07 13:13:32 +0530

    Create users table and add user endpoints

---
+++ app/database.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}' 
+#"postgresql://user:password@postgresserver/db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='chess', user='postgres', password='postgres', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        break
+    except Exception as error:
+        print(error)
+        time.sleep(2)
---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from .routers import user, auth #game
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+
+# models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.include_router(game.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+
+@app.get("/")
+def root():
+    return {"message":"Server running"}
